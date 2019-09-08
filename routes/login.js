9fc7ebf64b5adf4f2d4c5078017365c7dc2c2784
Abdifatah.206 <shire2567@gmail.com>
2019-09-07 21:09:49 -0700

    minor change

--- routes/login.js
+++ routes/login.js
@@ -2,6 +2,13 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 
+const mysql = require('mysql');
+
+
+
+
+
+
 router.get('/', function(request, response) {
 	response.sendFile(path.join(__dirname + '/signin.html'));
 });
@@ -12,31 +19,28 @@ router.post('/signin', function(request, response) {
 	if (username && password) {
 		db.query('SELECT * FROM logs WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
 			if (results.length > 0) {
-					request.session.loggedin = true;
-					request.session.username = username;
-					response.redirect('/');
-				} else {
-					response.send('Incorrect Username and/or Password!');
-				}
-				response.end();
-			});
-		} else {
-			response.send('Please enter Username and Password!');
+				request.session.loggedin = true;
+				request.session.username = username;
+				response.redirect('/');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}
 			response.end();
-		}
-	});
-
-	router.get('/', function(request, response) {
-		if (request.session.loggedin) {
-			response.send('Welcome back, ' + request.session.username + '!');
-		} else {
-			response.send('Please login to view this page!');
-		}
+		});
+	} else {
+		response.send('Please enter Username and Password!');
 		response.end();
-	});
+	}
+});
 
-
-
+router.get('/', function(request, response) {
+	if (request.session.loggedin) {
+		response.send('Welcome back, ' + request.session.username + '!');
+	} else {
+		response.send('Please login to view this page!');
+	}
+	response.end();
+});
 
 
 module.exports = router;
