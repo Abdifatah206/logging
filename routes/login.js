51dd6caf33ddfda966658e454ca145ccf40e05ca
Abdifatah.206 <shire2567@gmail.com>
2019-11-28 23:29:26 -0800

    loging page color changed

--- routes/login.js
+++ routes/login.js
@@ -16,7 +16,7 @@ router.post('/signin', function(request, response) {
 			if (results.length > 0) {
 				request.session.loggedin = true;
 				request.session.username = username;
-				response.redirect('/api/v1/register');
+				response.redirect('/');
 			} else {
 				response.send('Incorrect Username and/or Password!');
 			}
